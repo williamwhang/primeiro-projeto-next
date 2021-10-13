4fceab4a63895a015823fe5c259d1b1d675ebf15
williamwhang <whang.william@gmail.com>
2021-10-13 16:31:58 -0300

    Salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -10,4 +13,26 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  }
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+      
+      try { 
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email} }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    
+    },
+  }
 })
