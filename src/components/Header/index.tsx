ac1dd05b07da91590a9e744e0938b39c9c895624
williamwhang <whang.william@gmail.com>
2021-10-26 13:18:37 -0300

    Componente: ActiveLink - cloneElement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,19 +3,22 @@ import Link from 'next/link'
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+ 
+
     return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
           <img src="/images/logo.svg" alt="ig.news" />
           <nav>
-            <Link href="/" > 
-            <a className={styles.active}>Home</a>
-            </Link> 
-            <Link href="/posts"> 
+            <ActiveLink activeClassName={styles.active} href="/" > 
+            <a>Home</a>
+            </ActiveLink> 
+            <ActiveLink activeClassName={styles.active} href="/posts"> 
             <a>Posts</a>
-            </Link> 
+            </ActiveLink> 
           </nav>
 
           <SignInButton />
