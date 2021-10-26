afd08e85765bc7731bb00c778e5d45d9c2210b3c
williamwhang <whang.william@gmail.com>
2021-10-26 09:57:56 -0300

    Navegação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link'
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -7,8 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
           <img src="/images/logo.svg" alt="ig.news" />
           <nav>
+            <Link href="/" > 
             <a className={styles.active}>Home</a>
+            </Link> 
+            <Link href="/posts"> 
             <a>Posts</a>
+            </Link> 
           </nav>
 
           <SignInButton />
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -50,7 +50,6 @@ export const getStaticProps: GetStaticProps = async () => {
         pageSize: 100,
     })
 
-
     const posts = response.results.map(post => {
         return {
             slug: post.uid,
